12e73d8b5db4ee191380a1462b862891dce5599d
Hehh8 <2424952886@qq.com>
2022-07-15 17:07:57 +0800

    push test

--- src/main/java/com/example/webapp/HelloResource.java
+++ src/main/java/com/example/webapp/HelloResource.java
@@ -18,5 +18,6 @@ public class HelloResource {
         System.out.println("hello git3");
         System.out.println("master test");
         System.out.println("branch test");
+        System.out.println("push test");
     }
 }
